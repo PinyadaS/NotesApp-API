85b8c314d8f7b0676db21f319ffb97a4026f44f8
PinyadaS <155807141+PinyadaS@users.noreply.github.com>
2024-01-13 16:17:05 +0700

    finished login
    find username and password and generate jwt token

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,5 +1,8 @@
 const { response } = require("express")
-const { UserModel } = require("../models/user")
+const { UserModel, User } = require("../models/user")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 class UserController {
     //register logic
@@ -9,6 +12,28 @@ class UserController {
             const newUser = await UserModel.createUser(user)
             newUser.password = undefined
             return res.status(201).json({data:newUser})
+        } catch (error) {
+            return res.status(500).json({data:error.message})
+        }
+    }
+
+    //login logic
+    async loginUser(req,res) {
+        try {
+            const {username,password} = req.body
+            const user = await User.findOne({where: {username: username}})
+            if (!user) {
+                return res.status(404).json({data:"user not found"})
+            }
+            bcrypt.compare(password,user.password,(err,result) => {
+                if (err) {
+                    return res.status(400).json({data:"password incorrect"})
+                }
+                if (result) {
+                    const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET, {expiresIn: "30d"})
+                    return res.status(200).json({data:`Bearer ${token}`})
+                }
+            })
         } catch (error) {
             return res.status(500).json({data:error.message})
         }
--- routes/user_route.js
+++ routes/user_route.js
@@ -2,8 +2,12 @@ const express = require ('express');
 const UserController = require('../controller/user_controller');
 const router = express.Router();
 
-router.post('/register', async(req,res) => {
-    await UserController.registerUser(req,res)
+router.post('/register', (req,res) => {
+    UserController.registerUser(req,res)
 });
+
+router.post('/login', (req,res) => {
+    UserController.loginUser(req,res)
+})
 
 module.exports = router;
